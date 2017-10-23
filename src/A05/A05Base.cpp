fe185429f7627b5d4107cd2388a5b94556b02ab4
Ramon Mormeneo <almiond6@gmail.com>
2017-10-23 10:18:49 +0200

    azhsd

--- src/A05/A05Base.cpp
+++ src/A05/A05Base.cpp
@@ -10,6 +10,7 @@
 
 int main(int, char*[]) {
 
+	
 	// --- INIT ---
 	
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) throw "No es pot inicialitzar SDL subsystems";
@@ -29,15 +30,17 @@ int main(int, char*[]) {
 	if (renderer == nullptr) throw "No es pot inicialitzar SDL_Renderer";
 
 	// --- SPRITES ---
+
 	SDL_Texture *bgTexture{ IMG_LoadTexture(renderer, "../../res/img/bg.jpg") };
 	if (bgTexture == nullptr) throw " No s'han pogut carregar les textures";
 	SDL_Rect bgRect{ 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT };
 		// --- Animated Sprite ---
-	/*
+	
 	SDL_Texture *playerTexture{ IMG_LoadTexture (renderer,"../../res/img/kintoun.png") };
 	if (playerTexture == nullptr) throw " No s'han pogut crear les textures";
-	SDL_Rect playerRect{ 0, 0, 350, 189 };
-	SDL_Rect playerTarget{ 0, 0, 100, 100 };*/
+	SDL_Rect playerRect{ 0, 0, 100, 50 };
+	SDL_Rect playerTarget{ 0, 0, 100, 100 };
+	/*
 	SDL_Texture *playerTexture(IMG_LoadTexture(renderer, "../../res/img/sp01.png"));
 	SDL_Rect playerRect, playerPosition;
 	int textWidth, textHeight, frameWidth, frameHeight;
@@ -48,15 +51,19 @@ int main(int, char*[]) {
 	playerRect.x = playerRect.y = 0;
 	playerPosition.h = playerRect.h = frameHeight;
 	playerPosition.w = playerRect.w = frameWidth;
-	int frameTime = 0;
+	int frameTime = 0;*/
 	// --- TEXT ---
 	TTF_Font *font{ TTF_OpenFont("../../res/ttf/saiyan.ttf",80) };
 	if(font==nullptr)throw "No es pot inicialitzar SDL_ttf font";
-	SDL_Surface *tmpSurf{ TTF_RenderText_Blended(font,"My first SDL game",SDL_Color{255,150,0,255} ) };
-	if (tmpSurf == nullptr)throw "No es pot inicialitzar SDL_ttf font";
-	SDL_Texture *textTexture{ SDL_CreateTextureFromSurface(renderer,tmpSurf) };
-	SDL_Rect textRect{ 100,50,tmpSurf->w,tmpSurf->h };
-	SDL_FreeSurface(tmpSurf);
+	SDL_Surface *tmpSurf1{ TTF_RenderText_Blended(font,"Play",SDL_Color{0,255,0,255} ) };
+	SDL_Surface *tmpSurf2{ TTF_RenderText_Blended(font,"Exit",SDL_Color{255,0,0,255}) };
+	if (tmpSurf1 == nullptr||tmpSurf2==nullptr)throw "No es pot inicialitzar SDL_ttf font";
+	SDL_Texture *textTexture1{ SDL_CreateTextureFromSurface(renderer,tmpSurf1) };
+	SDL_Texture *textTexture2{ SDL_CreateTextureFromSurface(renderer,tmpSurf2) };
+	SDL_Rect textRect1{ 350,150,tmpSurf1->w,tmpSurf1->h };
+	SDL_Rect textRect2{ 350,250,tmpSurf2->w,tmpSurf2->h };
+	SDL_FreeSurface(tmpSurf1);
+	SDL_FreeSurface(tmpSurf2);
 	TTF_CloseFont(font);
 	// --- AUDIO ---
 	if (Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, 2, 1024) == -1)
@@ -70,49 +77,140 @@ int main(int, char*[]) {
 	// --- GAME LOOP ---
 	SDL_Event event;;
 	bool isRunning = true;
-	
-	while (isRunning){
-		// HANDLE EVENTS
-		while (SDL_PollEvent(&event)) {
-			switch (event.type) {
-			case SDL_QUIT:		isRunning = false; break;
-			case SDL_KEYDOWN:	if (event.key.keysym.sym == SDLK_ESCAPE) isRunning = false; break;
-			//case SDL_MOUSEMOTION: playerTarget.x = event.motion.x - playerRect.w/2; playerTarget.y = event.motion.y - playerRect.h/2; break;
-			default:;
-			}
-		}
-
-		// UPDATE
-		frameTime++;
-		if (FPS / frameTime <= 9)
+	bool OnMenu = true;
+	bool destroyMenu = false;
+	bool startGame = false;
+	while (isRunning)
+	{
+		if(OnMenu)
 		{
-			frameTime = 0;
-			playerRect.x += frameWidth;
-			if (playerRect.x >= textWidth)
-				playerRect.x = 0;
-		}
-		/*playerRect.x += (playerTarget.x - playerRect.x) / 10;
-		playerRect.y += (playerTarget.y - playerRect.y) / 10;*/
-		// DRAW
-			//Background
-		SDL_RenderClear(renderer);
-		SDL_RenderCopy(renderer, bgTexture, nullptr, &bgRect);
-		SDL_RenderCopy(renderer, textTexture, nullptr, &textRect);
-		//SDL_RenderCopy(renderer, playerTexture, nullptr, &playerRect);
-		
+			// HANDLE EVENTS
+			while (SDL_PollEvent(&event)) {
+				switch (event.type) {
+				case SDL_QUIT:		isRunning = false; break;
+				case SDL_KEYDOWN:	if (event.key.keysym.sym == SDLK_ESCAPE) isRunning = false; break;
+				case SDL_MOUSEMOTION: playerTarget.x = event.motion.x - playerRect.w / 2; playerTarget.y = event.motion.y - playerRect.h / 2; break;
+				case SDL_MOUSEBUTTONDOWN: if (event.motion.x >= 350 && event.motion.y >= 150 && event.motion.x <= 350 + tmpSurf1->w&& event.motion.y <= 150 + tmpSurf1->h) { OnMenu = false; startGame = true; }if (event.motion.x >= 350 && event.motion.y >= 250 && event.motion.x <= 350 + tmpSurf1->w&& event.motion.y <= 250 + tmpSurf1->h) isRunning = false;	break;
+				default:;
+				}
+			}
+			// UPDATE
+			/*frameTime++;
+			if (FPS / frameTime <= 9)
+			{
+				frameTime = 0;
+				playerRect.x += frameWidth;
+				if (playerRect.x >= textWidth)
+					playerRect.x = 0;
+			}*/
+			playerRect.x += (playerTarget.x - playerRect.x) / 10;
+			playerRect.y += (playerTarget.y - playerRect.y) / 10;
+			// DRAW
+				//Background
+			SDL_RenderClear(renderer);
+			SDL_RenderCopy(renderer, bgTexture, nullptr, &bgRect);
+			SDL_RenderCopy(renderer, textTexture1, nullptr, &textRect1);
+			SDL_RenderCopy(renderer, textTexture2, nullptr, &textRect2);
+			SDL_RenderCopy(renderer, playerTexture, nullptr, &playerRect);
+
 			//Animated Sprite
-		SDL_RenderCopy(renderer, playerTexture, &playerRect, &playerPosition);
-		SDL_RenderPresent(renderer);
+		//SDL_RenderCopy(renderer, playerTexture, &playerRect, &playerPosition);
+			SDL_RenderPresent(renderer);
+
+		}
+		
+		if (startGame)
+		{
+			// HANDLE EVENTS
+			while (SDL_PollEvent(&event)) {
+				switch (event.type) {
+				case SDL_QUIT:		isRunning = false; break;
+				case SDL_KEYDOWN:	
+					if (event.key.keysym.sym == SDLK_ESCAPE)
+					{
+						isRunning = false; 
+					}
+					if (event.key.keysym.sym == SDLK_UP)
+					{
+						if (playerTarget.y >= 0)
+						{
+							playerTarget.y -=1 ;
+						}
+					}
+					if (event.key.keysym.sym == SDLK_DOWN)
+					{
+						if (playerTarget.y <= SCREEN_HEIGHT-50)
+						{
+							playerTarget.y += 1;
+						}
+					}
+					else if (event.key.keysym.sym == SDLK_LEFT)
+					{
+						if (playerTarget.x >= 0)
+						{
+							playerTarget.x -= 1;
+						}
+					}
+					else if (event.key.keysym.sym == SDLK_RIGHT)
+					{
+						if (playerTarget.x <= SCREEN_WIDTH-100)
+						{
+							playerTarget.x += 1;
+						}
+					}
+					else if (event.key.keysym.sym == SDLK_w)
+					{
+						if (playerTarget.y >= 0)
+						{
+							playerTarget.y -= 1;
+						}
+					}
+					else if (event.key.keysym.sym == SDLK_s)
+					{
+						if (playerTarget.y <= SCREEN_HEIGHT - 50)
+						{
+							playerTarget.y += 1;
+						}
+					}
+					else if (event.key.keysym.sym == SDLK_a)
+					{
+						if (playerTarget.x >= 0)
+						{
+							playerTarget.x -= 1;
+						}
+					}
+					else 
+						if (event.key.keysym.sym == SDLK_d)
+					{
+						if (playerTarget.x <= SCREEN_WIDTH - 100)
+						{
+							playerTarget.x += 1;
+						}
+					}
+					break;
+				default:;
+				}
+			}
+			// UPDATE
+			playerRect.x += (playerTarget.x - playerRect.x) / 10;
+			playerRect.y += (playerTarget.y - playerRect.y) / 10;
+			SDL_RenderClear(renderer);
+			//DRAW
+			SDL_RenderCopy(renderer, bgTexture, nullptr, &bgRect);
+			SDL_RenderCopy(renderer, playerTexture, nullptr, &playerRect);
+			SDL_RenderPresent(renderer);
+		
+		}
+	
 
 	}
-
 	// --- DESTROY ---
-	SDL_DestroyTexture(bgTexture);
 	SDL_DestroyTexture(playerTexture);
-	SDL_DestroyTexture(textTexture);
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
+	SDL_DestroyTexture(textTexture1);
+	SDL_DestroyTexture(textTexture2);
 	Mix_CloseAudio();
+	destroyMenu = false;
+
 	// --- QUIT ---
 	SDL_Quit();
 	IMG_Quit();
